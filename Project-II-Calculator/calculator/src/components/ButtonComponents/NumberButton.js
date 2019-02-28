0f53edd34e0e239e706dd100149183cfd6e28924
will Ediger <will.ediger@gmail.com>
2019-02-28 01:29:36 -0600

    created NumberButton component built on GenericButton

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+import GenericButton from "./GenericButton";
+
+const NumberButton = props => (
+  <GenericButton buttonStyle={`number-button ${props.buttonStyle}`} text={props.text} />
+);
+  
+export default NumberButton;
