da2d4ab1473587d92f8c71659571f38d04413d00
will Ediger <will.ediger@gmail.com>
2019-02-28 01:32:30 -0600

    added display, clearbutton, numpad, and operator components to calculator app

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ClearButton from './components/ButtonComponents/ClearButton';
+import Numpad from './components/GroupComponents/Numpad';
+import Operators from './components/GroupComponents/Operators';
+
+const App = () => (
+  <div className="container">
+    <CalculatorDisplay text="0"/>
+    <div class="input">
+      <div className="clear-numpad">
+        <ClearButton/>
+        <Numpad />
+      </div>
+      <Operators />
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
