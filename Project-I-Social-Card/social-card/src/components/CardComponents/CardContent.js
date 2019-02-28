f1b3160e5ba47e64bbced0b4edd208e9ddc441d1
will Ediger <will.ediger@gmail.com>
2019-02-27 22:31:20 -0600

    fixed card content text to match design file

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,7 +5,7 @@ const CardContent = () => (
   <div className="card-content">
     <h3>Get started with React</h3>
     <p>
-      Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!
+      React makes it painless to create interactive UIs. Design simple views for each state in your application.
     </p>
     <small>reactjs.org</small>
   </div>
